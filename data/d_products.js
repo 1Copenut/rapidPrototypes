659d9c53d2f3db91571c13a345d64e5a8627ae18
Trevor <trevor.pierce@continuumdesign.net>
2013-05-19 21:12:27 -0600

    Successfully abstracted the data files
    It's possible to create one large object literal, and export it from its own file. This creates a lot of new possibilities for abstraction and quasi-realistic model nesting.

---
+++ data/d_products.js
@@ -0,0 +1,60 @@
+var products = {
+	"101": {
+		id: 101,
+		title: "Las Dos Fridas",
+		artist: 1,
+		price: "$59.99",
+		info: [
+			'11" x 14"',
+			"acid-free paper",
+			"suitable for matting",
+			"limited edition"
+		]
+	},
+	"102": {
+		id: 102,
+		title: "Two Acrobats with a Dog",
+		artist: 2,
+		price: "$49.99",
+		info: [
+			'8" x 11"',
+			"acid-free paper",
+			"suitable for matting"
+		]
+	},
+	"103": {
+		id: 103,
+		title: "Madame Pompadour",
+		artist: 3,
+		price: "$29.99",
+		info: [
+			'5" x 7"',
+			"acid-free paper",
+			"matted"
+		]
+	},
+	"104": {
+		id: 104,
+		title: "Dissonance",
+		artist: 4,
+		price: "$39.99",
+		info: [
+			'8" x 11"',
+			"acid-free paper",
+			"suitable for matting"
+		]
+	},
+	"105": {
+		id: 105,
+		title: "Irene and Her Sister",
+		artist: 5,
+		price: "$39.99",
+		info: [
+			'5" x 7"',
+			"acid-free paper",
+			"matted"
+		]
+	}
+};
+
+exports.products = products;
