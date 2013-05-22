e0eccce495c97744198c6d4f4b5f753de93101b5
Trevor Pierce <tpierce@bcgtrans.com>
2013-05-22 16:57:55 -0600

    Trimming extra IIFE declarations
    Require.js has a much more streamlined functionality, so I've removed a lot of the module-based code that's not required.

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,54 @@
-(function () {
+/*global define */
+/*global Hogan */
+
+/*************************************************
+                  MAIN MODULE
+**************************************************/
+
+define([
+	"jquery",
+	"hogan"
+], function($) {
 	"use strict";
-	
-	define([
-		"jquery",
-		"hogan"
-	], function($) {
 
-		var resultsTmpl,
-				detailTmpl;
+	var resultsTmpl,
+			detailTmpl;
 
-		function init() {
-			$.get( "results", function( data ) {
-				$( "div.results" ).html(resultsTmpl.render(data));
-				console.log(data);
-			});
-			$( "div.content" ).on( "click", "div.result", showDetail );
+	function init() {
+		$.get( "results", function( data ) {
+			$( "div.results" ).html(resultsTmpl.render(data));
+		});
+		$("div.content").on("click", "div.result", showDetail);
+	}
+
+	function showDetail(e) {
+		var id = e.target.parentNode.getAttribute("data-id");
+		loadDetail( id );
+		$("div.detail").show();
+	}
+
+	function loadDetail(id) {
+		if (id.data) {
+			id = id.data.id;
 		}
+		$.get("detail/" + id, function(data) {
+			$("div.detail").html(detailTmpl.render(data));
+			$("div.detail a.prev").on( "click", { id: parseInt(id, 10) - 1 }, loadDetail);
+			$("div.detail a.next").on( "click", { id: parseInt(id, 10) + 1 }, loadDetail);
+			$("div.detail div.artist a").on( "click", loadBio);
+		});
+	}
 
-		function showDetail( e ) {
-			var id = $( this ).data( "id" );
-			loadDetail( id );
-			$( "div.detail" ).show();
-		}
+	function loadBio( artistId ) {
+		// some stuff here, definitely needs a template
+	}
 
-		function loadDetail( id ) {
-			if ( id.data ) {
-				id = id.data.id;
-			}
-			$.get( "detail/" + id, function(data) {
-				$( "div.detail" ).html(detailTmpl.render(data));
-				$( "div.detail a.prev" ).on( "click", { id: parseInt( id ) - 1 }, loadDetail );
-				$( "div.detail a.next" ).on( "click", { id: parseInt( id ) + 1 }, loadDetail );
-				$( "div.detail div.artist a" ).on( "click", loadBio );
-			});
-		}
-
-		function loadBio( artistId ) {
-			// some stuff here, definitely needs a template
-		}
-
-		$.when( 
-			$.get( "tmpl/results.hogan", function( tmpl ) {
-				resultsTmpl = Hogan.compile( tmpl );
-			}, "text" ),
-			$.get( "tmpl/detail.hogan", function( tmpl ) {
-				detailTmpl = Hogan.compile( tmpl );
-			}, "text" )
-		).then( init );
-	});
-}());
-
-
+	$.when( 
+		$.get("tmpl/results.hogan", function(tmpl) {
+			resultsTmpl = Hogan.compile(tmpl);
+		}, "text"),
+		$.get("tmpl/detail.hogan", function(tmpl) {
+			detailTmpl = Hogan.compile(tmpl);
+		}, "text")
+	).then(init);
+});
